a27e7768cd2869406c720b87663e2d8fc1391766
GuilhermeSavioRibas <guilherme.savio@gmail.com>
2022-06-17 14:04:47 -0400

    modulos

---
+++ Módulo II - HTML-CSS-JS/javascript/modulos/atividade/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Guilherme", "32"));
+console.log(mostraCidade("Guilherme", "New York"));
+console.log(mostraHobby("Guilherme", "progamar"));
