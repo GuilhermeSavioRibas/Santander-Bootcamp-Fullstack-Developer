d873421b47c15aeb4475d4ba6307eeb87324e09c
GuilhermeSavioRibas <guilherme.savio@gmail.com>
2022-06-25 10:36:18 -0400

    contador

---
+++ Módulo II - HTML-CSS-JS/javascript/introducao/contador/assets/scripts.js
@@ -0,0 +1,16 @@
+let count = 0;
+const CURRENT_NUMBER = document.getElementById('currentNumber');
+
+function increment() {
+	if (count < 10) {
+		count++;
+		CURRENT_NUMBER.innerHTML = count;
+	}
+}
+
+function decrement() {
+	if (count > -10) {
+		count--;
+		CURRENT_NUMBER.innerHTML = count;
+	}
+}
